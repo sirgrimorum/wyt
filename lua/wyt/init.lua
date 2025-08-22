97e4dab6bf35489e7d4a6d03b8cc832edc85c25b
SirGrimorum <andres.espinosa@grimorum.com>
2025-08-21 20:19:25 -0500

    feat: multi_select with telescope

--- lua/wyt/init.lua
+++ lua/wyt/init.lua
@@ -4,6 +4,9 @@ local autocmd = require("wyt.autocmd")
 local M = {}
 
 function M.setup(opts)
+    if not pcall(require, "telescope") then
+        print("[WYT] Warning: telescope.nvim is required for multi-selection.\n")
+    end
     commands.setup()
     autocmd.setup()
 end
